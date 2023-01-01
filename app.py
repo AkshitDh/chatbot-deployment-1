6bd653871a85fe6ebea0520a60480f471816184c
Akshit <akshitdhoundiyal@gmail.com>
2023-01-01 18:21:21 +0530

    My first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from chat import get_response
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    text = request.get_json().get("message")
+    print(text)
+    return "hello"
+
+
+@app.route('/predict', methods=["POST", "GET"])
+def predict():
+    text = request.get_json().get("message")
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
